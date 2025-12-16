1af88c447ebacf664e6315faeb8bd8ffb95dd53f
Bruno Silva <92815660+brunoF-Silva@users.noreply.github.com>
2025-12-16 17:37:21 -0300

    feat: add initial project and app structure
    ## What it does:
    This PR adds the basic structure of the project.
    
    ## Files and Folders:
    All the essential files and folders.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import CalendarApp from "./Components/CalendarApp"
+import "./Components/CalendarApp.css"
+
+const App = () => {
+  return <div>
+    <CalendarApp />
+    </div>
+}
+export default App
