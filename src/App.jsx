d8b6010594e4b36cfe60e500e712bf50f53eec90
Bruno Silva <92815660+brunoF-Silva@users.noreply.github.com>
2025-12-17 11:46:37 -0300

    feat: add core jsx files
    ## What it does:
    This commit adds all the core JSX files.
    
    ##Files and Folders:
    `CalendarApp.jsx` - creates a static version of the calendar component. 
    `App.jsx` - renders the main calendar component in our app.
    `index.html` - imports the boxicons library.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import CalendarApp from "./Components/CalendarApp"
 import "./Components/CalendarApp.css"
 
 const App = () => {
-  return <div>
+  return <div className="container">
     <CalendarApp />
     </div>
 }
